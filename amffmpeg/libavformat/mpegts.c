59494e55a316fa1abcdc02d183dd5cebc2f713fc
davilla <davilla@4pi.com>
2013-01-06 13:12:51 -0500

    [aml] merge 0016-Speed-up-mpegts-av_find_stream_info.patch

--- amffmpeg/libavformat/mpegts.c
+++ amffmpeg/libavformat/mpegts.c
@@ -865,7 +865,7 @@ static int mpegts_push_data(MpegTSFilter *filter,
                         goto skip;
 
                     /* stream not present in PMT */
-                    if (!pes->st) {
+                    if (ts->auto_guess && !pes->st) {
                         pes->st = av_new_stream(ts->stream, pes->pid);
                         if (!pes->st)
                             return AVERROR(ENOMEM);
@@ -1985,7 +1985,10 @@ reget_packet_size:
 
         av_dlog(ts->stream, "tuning done\n");
 
-        s->ctx_flags |= AVFMTCTX_NOHEADER;
+        /* only flag NOHEADER if we are in file mode,
+           in streaming mode scanning may take too long for users */
+        if (!url_is_streamed(pb))
+            s->ctx_flags |= AVFMTCTX_NOHEADER;
     } else {
         AVStream *st;
         int pcr_pid, pid, nb_packets, nb_pcrs, ret, pcr_l;
